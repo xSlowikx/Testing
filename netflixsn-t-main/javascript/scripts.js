217c7dce661b99ced152aa41558ab3d1668865e5
Slowik <aletogu00@gmail.com>
2022-07-02 19:58:54 -0300

    COMMIT 02/07/2022, MOBILE FIRST REALIZADO index y peliculas

---
+++ netflixsn-t-main/javascript/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    /* console.log("Entré"); */ //Entró, xd.
+    $("#buscador").keyup(function(){
+        /* console.log($(this).val()); */ //Funciona correctamente, escribe en consola el value del campo buscar.
+        $("section.contenidos article,h2").hide()
+        $("section.contenidos h2.resultados").slideDown();
+        /* $("p:contains("+$(this).val()+")"); */
+        $("a").filter("#"+$(this).val()).css('display','inline-block');
+    });
+});
