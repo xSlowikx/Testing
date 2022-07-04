94ed19f9243e29e09d5ad159bf4e196205836183
Slowik <aletogu00@gmail.com>
2022-07-04 10:09:43 -0300

    04-07 funcionando filtros series y peliculas

--- netflixsn-t-main/javascript/scripts.js
+++ netflixsn-t-main/javascript/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    
+    //Filtro de busqueda sin terminar
     /* console.log("Entré"); */ //Entró, xd.
     $("#buscador").keyup(function(){
         /* console.log($(this).val()); */ //Funciona correctamente, escribe en consola el value del campo buscar.
@@ -6,5 +8,25 @@ $(document).ready(function(){
         $("section.contenidos h2.resultados").slideDown();
         /* $("p:contains("+$(this).val()+")"); */
         $("a").filter("#"+$(this).val()).css('display','inline-block');
+    });
+    
+    //FILTRO SERIES - NAV INDEX
+    $("a.series").click(function(){
+        console.log("Entre");
+        if($("section.contenidos a:contains(.serie)")){
+            $("section.contenidos a.serie").css('display','inline-block');
+            $("section.contenidos a.pelicula").css('display','none');
+        } else {
+        }
+    });
+
+    //FILTRO PELICULAS - NAV INDEX
+    $("a.peliculas").click(function(){
+        console.log("Entre");
+        if($("section.contenidos a:contains(.pelicula)")){
+            $("section.contenidos a.pelicula").css('display','inline-block');
+            $("section.contenidos a.serie").css('display','none');
+        } else {
+        }
     });
 });
